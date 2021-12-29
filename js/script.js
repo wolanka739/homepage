a38ccf453f35d4604ac9411cbfbfd42c70092b2e
Karolina Wolan <wolanka739@gmail.com>
2021-12-29 15:23:37 +0100

    mistakes corrected

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@
 
     const toggleBackground = () => {
         const body = document.querySelector(".js-body");
-        const button__themeName = document.querySelector(".js-button__themeName");
+        const buttonThemeName = document.querySelector(".js-button__themeName");
 
         body.classList.toggle("body--dark");
-        button__themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
+        buttonThemeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
     };
 
     const init = () => {
